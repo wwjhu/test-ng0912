f06546c3ec5e3164fe08dd7f669a957dc19c1673
Joost Huizinga <jhu@woodwing.com>
2023-09-21 09:06:45 +0200

    Change test to use a creator function instead of globals

--- projects/lib-ng0912/src/lib/feature/feature.component.spec.ts
+++ projects/lib-ng0912/src/lib/feature/feature.component.spec.ts
@@ -3,19 +3,17 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { FeatureComponent } from './feature.component';
 
 describe('FeatureComponent', () => {
-  let component: FeatureComponent;
-  let fixture: ComponentFixture<FeatureComponent>;
+    function setupTest() {
+        TestBed.configureTestingModule({
+            declarations: [FeatureComponent],
+        });
+        const fixture = TestBed.createComponent(FeatureComponent);
+        fixture.detectChanges();
+        return { fixture };
+    }
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      declarations: [FeatureComponent]
+    it('should work', () => {
+        const { fixture } = setupTest();
+        expect(fixture.debugElement.nativeElement.textContent).toEqual('feature works!');
     });
-    fixture = TestBed.createComponent(FeatureComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
-
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
 });
