971254fd5975eddc04b7d428af4cd7609d90110c
Joost Huizinga <jhu@woodwing.com>
2023-09-21 09:09:36 +0200

    add test component, import feature module

--- projects/lib-ng0912/src/lib/feature/feature.component.spec.ts
+++ projects/lib-ng0912/src/lib/feature/feature.component.spec.ts
@@ -1,13 +1,19 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { FeatureComponent } from './feature.component';
+import { Component } from '@angular/core';
+import { FeatureModule } from './feature.module';
 
 describe('FeatureComponent', () => {
+    @Component({ template: '<lib-feature></lib-feature>' })
+    class TestFeatureComponent {}
+
     function setupTest() {
         TestBed.configureTestingModule({
-            declarations: [FeatureComponent],
+            declarations: [TestFeatureComponent],
+            imports: [FeatureModule],
         });
-        const fixture = TestBed.createComponent(FeatureComponent);
+
+        const fixture = TestBed.createComponent(TestFeatureComponent);
         fixture.detectChanges();
         return { fixture };
     }
